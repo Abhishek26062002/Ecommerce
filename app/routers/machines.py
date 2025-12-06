5d76f163230bb0f2e7b7aee8481834ee3ac3abde
Abhishek161515101 <120778142+Abhishek161515101@users.noreply.github.com>
2025-12-06 23:51:49 +0530

    new-21 commit

--- app/routers/machines.py
+++ app/routers/machines.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from typing import List
 from app.services.cloudinary import upload_file_to_cloudinary
+import uuid
 
 
 router = APIRouter(prefix="/machines", tags=["machines"])
@@ -26,7 +27,6 @@ async def get_machine(machine_id: str, db: AsyncSession = Depends(get_db)):
 
 @router.post("/add_machine")
 async def add_machine(
-    machine_id: str = Form(...),
     name: str = Form(...),
     price: float = Form(...),
     description: str = Form(None),
@@ -41,6 +41,7 @@ async def add_machine(
 ):
 
     # Check duplicate machine_id
+    machine_id = f'machine_{str(uuid.uuid4())}'
     existing = await db.execute(select(Machine).where(Machine.machine_id == machine_id))
     if existing.scalars().first():
         raise HTTPException(status_code=400, detail="Machine ID already exists")
@@ -69,3 +70,61 @@ async def add_machine(
     await db.refresh(new_machine)
 
     return {"message": "Machine added successfully", "machine": new_machine}
+
+
+
+
+@router.post("/update_machine")
+async def update_machine(
+    machine_id: str,
+    name: str = Form(None),
+    price: float = Form(None),
+    description: str = Form(None),
+    brand: str = Form(None),
+    machine_type: str = Form(None),
+    images : List[UploadFile] = File(None),
+    image_urls : List[str] = Form(None),
+    discount_price: float = Form(None),
+    color: str = Form(None),
+    size: str = Form(None),
+    stock_count: int = Form(None),
+    db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(Machine).where(Machine.machine_id == machine_id))
+    machine = result.scalars().first()
+    if not machine:
+        raise HTTPException(status_code=404, detail="Machine not found")
+
+    if name is not None:
+        machine.name = name
+    if price is not None:
+        machine.price = price
+    if description is not None:
+        machine.description = description
+    if brand is not None:
+        machine.brand = brand
+    if machine_type is not None:
+        machine.machine_type = machine_type
+    if images is not None:
+        images_urls = image_urls if image_urls is not None else []
+        for image in images:
+            print("Uploading image to Cloudinary")
+            upload_result = await upload_file_to_cloudinary(image)
+            print(f"Upload result: {upload_result}")
+            if upload_result:
+                print(f"Uploaded image URL: {upload_result['secure_url']}")
+                images_urls.append(upload_result["secure_url"])
+        machine.images_urls = images_urls
+    if discount_price is not None:
+        machine.discount_price = discount_price
+    if color is not None:
+        machine.color = color
+    if size is not None:
+        machine.size = size
+    if stock_count is not None:
+        machine.stock_count = stock_count
+
+    await db.commit()
+    await db.refresh(machine)
+
+    return {"message": "Machine updated successfully", "machine": machine}
